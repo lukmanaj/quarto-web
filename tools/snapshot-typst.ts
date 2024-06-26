7b49d19aeafb5236f60977e02103628ff540e204
Gordon Woodhull <gordon.woodhull@posit.co>
2024-06-26 03:23:30 -0400

    initial version of snapshot-typst.ts; improve gt-temps

---
+++ tools/snapshot-typst.ts
@@ -0,0 +1,88 @@
+import * as path from "https://deno.land/std/path/mod.ts";
+
+function getModuleDir(importMeta: ImportMeta): string {
+  return path.resolve(path.dirname(path.fromFileUrl(importMeta.url)));
+}
+
+function toAbsolute(p: string) {
+  return p[0] === '/' ? p :  path.normalize(path.join(Deno.cwd(), p));
+}
+
+const dir = getModuleDir(import.meta);
+
+ // todo: use quarto to find _quarto.yml and read outputDir from it
+const projectDir = path.normalize(path.join(dir, '..'));
+const outputDir = '_site';
+
+const args = Deno.args;
+if(args.length < 2 || args[0] == '-h') {
+  console.error('usage: typst-snapshot source.qmd dest.png')
+  console.error('  paths can be relative or relative but must be within a quarto project')
+  Deno.exit(1)
+}
+const sourcePath = toAbsolute(args[0]),
+  destPath = toAbsolute(args[1]);
+if(!sourcePath.startsWith(projectDir)) {
+  console.error('source path', sourcePath, 'is not within quarto project dir', projectDir);
+  Deno.exit(1);
+}
+if(!destPath.startsWith(projectDir)) {
+  console.error('dest path', destPath, 'is not within quarto project dir', projectDir);
+  Deno.exit(1);
+}
+
+const relSourcePath = sourcePath.slice(projectDir.length + 1);
+const dryRun = false;
+const qcmd = [
+  'render',
+  args[0],
+  '-M',
+  'echo:false',
+  '-M',
+  'warning:false',
+  '-M',
+  'title:false'
+];
+console.log('quarto', ...qcmd);
+if (!dryRun) {
+  const cmd = new Deno.Command('quarto', {
+      args: qcmd
+  });
+  const output = await cmd.output();
+  if (!output.success) {
+      console.log(new TextDecoder().decode(output.stderr));
+      Deno.exit(2);
+  }
+}
+
+const absPdfPath = path.join(projectDir, outputDir, relSourcePath.replace(/qmd$/, 'pdf')),
+  relPdfPath = path.relative('.', absPdfPath); // for readability
+
+const mcmd = [
+  'convert',
+  '-verbose',
+  '-density', 300,
+  '-trim',
+  '+repage',
+  relPdfPath,
+  '-quality', 100,
+  '-flatten',
+  '-units', 'PixelsPerInch',
+  '-set', 'density', 300,
+  args[1]
+];
+
+console.log('magick', ...mcmd);
+if (!dryRun) {
+  const cmd = new Deno.Command('magick', {
+      args: mcmd
+  });
+  const output = await cmd.output();
+  if (!output.success) {
+      console.log(new TextDecoder().decode(output.stderr));
+      Deno.exit(2);
+  }
+}
+
+// quarto render  -M keep-typ:true -M echo:false -M warning:false -M title:false
+// magick convert -verbose -density 300 -trim +repage file pandas-confusion-matrix.pdf -quality 100 -flatten -sharpen 0x1.0 pandas-cf.png
